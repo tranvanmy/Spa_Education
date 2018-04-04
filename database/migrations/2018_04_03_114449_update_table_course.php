ae1df675afb3dd9ccd7a2ef32c61e8f43186c971
thangtt <trungthangngo@gmail.com>
2018-04-04 08:45:49 +0700

    fix migrate course

--- database/migrations/2018_04_03_114449_update_table_course.php
+++ database/migrations/2018_04_03_114449_update_table_course.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnCours extends Migration
+class UpdateTableCourse extends Migration
 {
     public function up()
     {
